db3778c25d237669bea127249a3715250ccd93af
Hasan Uddin <abcnizamd@gmail.com>
2026-02-23 21:35:58 +0600

    update

--- src/libs/todo/components/todo-table/todo-table.component.ts
+++ src/libs/todo/components/todo-table/todo-table.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common'
 import { Component, inject } from '@angular/core'
 import { TodoListStateService } from '../../todo-list-state.service'
+import { ButtonModule } from 'primeng/button'
+import { TableModule } from 'primeng/table'
 
 @Component({
     selector: 'app-todo-table',
-    imports: [CommonModule],
+    imports: [CommonModule, ButtonModule, TableModule],
     templateUrl: './todo-table.component.html',
     styleUrl: './todo-table.component.css',
 })
