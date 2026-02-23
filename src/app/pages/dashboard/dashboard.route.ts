a3de2d96d462b15542bdfdb3d72e3b8a0cc8fd4c
Hasan Uddin <abcnizamd@gmail.com>
2026-02-23 22:37:56 +0600

    add Event Type to Route

--- src/app/pages/dashboard/dashboard.route.ts
+++ src/app/pages/dashboard/dashboard.route.ts
@@ -3,6 +3,7 @@ import { PageLayout, setLayout } from '../../../libs/common-components'
 
 export type DashboardRoutes = {
     bookings: Route
+    eventType: Route
 }
 
 export const dashboardRoutes: DashboardRoutes = {
@@ -11,6 +12,14 @@ export const dashboardRoutes: DashboardRoutes = {
         loadComponent: () =>
             import('./page-booking/page-bookings.component').then(
                 (m) => m.PageBookingsComponent,
+            ),
+        resolve: { layout: setLayout(PageLayout.Dashboard) },
+    },
+    eventType: {
+        path: 'host/event-types',
+        loadComponent: () =>
+            import('./page-event-types/page-event-types.component').then(
+                (m) => m.PageEventTypesComponent,
             ),
         resolve: { layout: setLayout(PageLayout.Dashboard) },
     },
